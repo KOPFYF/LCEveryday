33b88a17f7734199931ed84b78132dce65631435
KOPFYF <fei8@purdue.edu>
2020-11-17 13:52:04 -0500

    bitmask

--- Dynamic Programming/distributeRepeatingIntegers1655.py
+++ Dynamic Programming/distributeRepeatingIntegers1655.py
@@ -1,4 +1,66 @@
-class Solution(object):
+class Solution:
+    def canDistribute(self, nums: List[int], quantity: List[int]) -> bool:  
+        # DP O(50*3^m)
+        # Iterating through all masks with their submasks. Complexity O(3n)
+        # https://cp-algorithms.com/algebra/all-submasks.html
+        d = Counter(nums)
+        cnt = sorted(d.values())  
+        n, m = len(cnt), len(quantity)  
+        sums = defaultdict(int)
+        
+        # get sum for a subset of consumers
+        for mask in range(1, 1 << m):
+            for i in range(m):
+                if (1 << i) & mask:
+                    sums[mask] += quantity[i]   
+            # print(mask, sums[mask], bin(sums[mask])[2:])
+            
+        dp = [[0] * (n + 1) for _ in range(1 << m)]
+        dp[0][0] = 1
+        for mask in range(1 << m):
+            for i in range(n):
+                dp[mask][i + 1] |= dp[mask][i]
+                cur = mask
+                while cur:
+                    if (sums[cur] <= cnt[i] and dp[mask ^ cur][i]):
+                        dp[mask][i + 1] = 1
+                    cur = (cur - 1) & mask
+        return dp[-1][n]
+
+class Solution2:
+    def canDistribute(self, nums: List[int], quantity: List[int]) -> bool:  
+        # DP O(50*3^m)
+        # Iterating through all masks with their submasks. Complexity O(3n)
+        # https://cp-algorithms.com/algebra/all-submasks.html
+        d = Counter(nums)
+        cnt = sorted(d.values())  
+        n, m = len(cnt), len(quantity)  
+        sums = defaultdict(int)
+        
+        # get sum for a subset of consumers
+        for mask in range(1, 1 << m):
+            for i in range(m):
+                if (1 << i) & mask:
+                    sums[mask] += quantity[i]   
+            # print(mask, sums[mask], bin(sums[mask])[2:])
+        
+        # dp[mask][i]: if we can satisfy customers mask using first i numbers
+        @lru_cache(None)
+        def dfs(i, mask):      
+            if not mask: return True  
+            if i == n: return False  
+            
+            submask = mask      
+            while submask:          
+                if sums[submask] <= cnt[i] and dfs(i + 1, mask ^ submask):
+                    return True           
+                submask = (submask-1) & mask
+            return dfs(i + 1, mask)
+                
+        return dfs(0, (1 << m) - 1)
+
+
+class Solution1(object):
     def canDistribute(self, nums, quantity):
         """
         :type nums: List[int]
