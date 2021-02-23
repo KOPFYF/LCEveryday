afa66010aea6e35986c22bdb49d71a99fd763278
KOPFYF <fei8@purdue.edu>
2021-02-23 11:13:58 -0500

    beautiful arrangement

--- Backtracking/countArrangement526.py
+++ Backtracking/countArrangement526.py
@@ -20,3 +20,36 @@ class Solution:
             return res
         
         return dfs(nums, 1)     
+        
+
+        # dp bitmask, 1 <= N <= 15, subset, O(n*2^n)/O(2^n)
+        # mask: full -> zero, i: 1->n
+        # if mask has n, 1100, say n = 4, flip it to 0 -> 0100
+        @lru_cache(None)
+        def dfs(mask, i):
+            if i == N + 1: return 1
+            if not mask: return 0 # base case, mask = 0000
+            
+            res = 0
+            for n in range(1, N+1): # try from 1 to N for index i
+                if mask & (1 << (n-1)) and (not i % n or not n % i):
+                    res += dfs(mask ^ (1 << (n-1)), i+1)
+            return res
+
+        return dfs((1 << N) - 1, 1)
+    
+
+        # dp subset
+        @lru_cache(None)
+        def dfs(nums):
+            l = len(nums)
+            if l == 1: # Any integer can be divide by 1
+                return 1
+            res = 0
+            for j in range(l):
+                if nums[j] % l == 0 or l % nums[j] == 0:
+                    # print(nums[j], l)
+                    res += dfs(nums[:j] + nums[j+1:])
+            return res
+        
+        return dfs(tuple(range(1, n+1))) 
